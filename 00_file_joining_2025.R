b48d971b9007364eb82430bc15312b8c6ed5b04a
Owen Broadwater <owenbroadwater2028@u.northwestern.edu>
2026-02-04 20:57:52 -0600

    Translated files for 2025 joined

--- 00_file_joining_2025.R
+++ 00_file_joining_2025.R
@@ -1,14 +1,42 @@
 library(tidyverse)
 library(readxl)
+library(hms)
 
-path <- "Translated_Files"
 
-list.files(path)
+# rbind(list.files(path)[[1]], list.files(path)[[2]])
+# 
+# rbind(read_excel("Translated_Files/20250322_Aoka_Yayan_Uhit_Radi_FL_translated.xlsx"), read_excel("Translated_Files/20250404_Aoka_Uhit_FL_Translated.xlsx") |> 
+#         rename("Focal activity start time" = `focal activity start time`,
+#                "Time of arrival" = `Time of Arrival`,
+#                "Species of tree" = `Species of Tree`))
 
-rbind(list.files(path)[[1]], list.files(path)[[2]])
 
-rbind(read_excel(paste0(path, "/",list.files(path)[[1]])), read_excel(paste0(path, "/",list.files(path)[[2]])))
-read_excel(paste0(path, "/",list.files(path)[[1]]))
+#colnames(read_excel("Translated_Files/20250322_Aoka_Yayan_Uhit_Radi_FL_translated.xlsx")) == colnames(read_excel("Translated_Files/20250404_Aoka_Uhit_FL_Translated.xlsx"))
+
+test <- read_excel("Translated_Jan_Feb_March/20250315_Ewat_Herman_Rama_NN_translated.xlsx", col_types = "text")
+
+res <- read_excel("Translated_Jan_Feb_March/20250315_Ewat_Herman_Rama_NN_translated.xlsx", col_types = "text") |> 
+  mutate(Date = date(as_datetime(as.numeric(Date) * 86400,
+                            origin = "1899-12-30")),
+         `Time of arrival` = as_datetime(as.numeric(`Time of arrival`) * 86400,
+                                         origin = Date),
+         `Focal start time` = as_datetime(as.numeric(`Focal start time`) * 86400,
+                                         origin = Date),
+         `Focal activity start time` = as_datetime(as.numeric(`Focal activity start time`) * 86400,
+                                         origin = Date),
+         `Feeding start time` = as_datetime(as.numeric(`Feeding start time`) * 86400,
+                                         origin = Date),
+         `Feeding end time` = as_datetime(as.numeric(`Feeding end time`) * 86400,
+                                         origin = Date),
+         `Start time feeding rate` = as_datetime(as.numeric(`Start time feeding rate`) * 86400,
+                                         origin = Date),
+         `End time feeding rate` = as_datetime(as.numeric(`End time feeding rate`) * 86400,
+                                                 origin = Date))
+
+
+#read_excel(paste0(path, "/",list.files(path)[[1]]))
+
+#col_nums_error <- list("20250415_Ewit_yayan_radi_rama_NN_TO_Translated.xlsx", "20250315_Ewat_Herman_Rama_NN_translated.xlsx")
 
 
 combine_translated <- function(folder_path){
@@ -17,7 +45,7 @@ combine_translated <- function(folder_path){
   files <- list.files(folder_path)
   first_file = TRUE
   for(file in files){
-    #print(file)
+    print(file)
     xl <- read_excel(paste0(folder_path, "/", file), col_types = "text")
     count_obs <- count_obs + nrow(xl)
     if (first_file){
@@ -25,13 +53,53 @@ combine_translated <- function(folder_path){
     }else{
       df_to_ret <- rbind(df_to_ret, xl)
     }
-    #print(df_to_ret)
     first_file <- FALSE
   }
   print(count_obs)
   return(df_to_ret)
 }
 
-data_2025 <- combine_translated(path)
+
+up_through_march <- combine_translated("Translated_Jan_Feb_March")
+rest <- combine_translated("Translated_Apr_May")
+data_2025 <- rbind(up_through_march, rest |> 
+        rename("Focal activity start time" = `focal activity start time`,
+               "Time of arrival" = `Time of Arrival`,
+               "Species of tree" = `Species of Tree`))
+
+data_2025_clean <- data_2025 |> 
+  mutate(Date = date(as_datetime(as.numeric(Date) * 86400,
+                                 origin = "1899-12-30")),
+         `Time of arrival` = as_datetime(as.numeric(`Time of arrival`) * 86400,
+                                         origin = Date),
+         `Focal start time` = as_datetime(as.numeric(`Focal start time`) * 86400,
+                                          origin = Date),
+         `Focal activity start time` = as_datetime(as.numeric(`Focal activity start time`) * 86400,
+                                                   origin = Date),
+         `Feeding start time` = as_datetime(as.numeric(`Feeding start time`) * 86400,
+                                            origin = Date),
+         `Feeding end time` = as_datetime(as.numeric(`Feeding end time`) * 86400,
+                                          origin = Date),
+         `Start time feeding rate` = as_datetime(as.numeric(`Start time feeding rate`) * 86400,
+                                                 origin = Date),
+         `End time feeding rate` = as_datetime(as.numeric(`End time feeding rate`) * 86400,
+                                               origin = Date),
+         Visibility = as.numeric(Visibility),
+         Activity = as.factor(Activity),
+         `Amount eaten` = as.numeric(`Amount eaten`),
+         `Tree canopy height` = as.factor(`Tree canopy height`)) |> 
+  janitor::clean_names()
+
+data_2025_clean <- data_2025_clean |> 
+  mutate(part_of_fruit_eaten = as.factor(part_of_fruit_eaten),
+         seed_eating_activity = as.factor(seed_eating_activity))
+
+data_2025 |> 
+  distinct(`Tree canopy height`)
+
+data_2025_clean |> 
+  distinct(tree_number)
 
 write_csv(data_2025, file = "data/translated_data_2025.csv")
+write_csv(data_2025_clean, file = "data/translated_data_2025_clean.csv")
+
